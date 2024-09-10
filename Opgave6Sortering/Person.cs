1b1eedfebb05b6fe36b60aaef4e5163b89e40a36
katrine <eaa23kch@students.eaaa.dk>
2024-09-10 11:49:15 +0200

    Started exercise 6, didn't finish

---
+++ Opgave6Sortering/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave6Sortering
+{
+    internal class Person
+    {
+        public Person(int age, double weight, string name) 
+        {
+            Age = age;
+            Weight = weight;
+            Name = name;
+        }
+        int Age { get; set; }
+        double Weight {  get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ Opgave6Sortering/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave6Sortering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personer = new List<Person>();
+            Person ole = new Person(100, 100, "Ole");
+            Person jens = new Person(90, 90, "Jens");
+            Person yrsa = new Person(80, 80, "Yrsa");
+            personer.Add(ole);
+            personer.Add(jens);
+            personer.Add(yrsa);
+        }
+    }
+}
