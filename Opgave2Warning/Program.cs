3321fc9c7c35a7e96e250ebea148b1b03da39221
katrine <eaa23kch@students.eaaa.dk>
2024-09-10 09:40:15 +0200

    Made exercise 3 with delegates

---
+++ Opgave2Warning/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave2Warning
+{
+    internal class Program
+    {
+        List<Warning> warnings = new List<Warning>();
+        static void Main(string[] args)
+        {
+            PowerPlant plant = new PowerPlant();
+            plant.SetWarning(WarningToConsole);
+            plant.SetWarning(WarningAgain);
+
+            for (int i = 0; i < 10; i++)
+            {
+                plant.HeatUp();
+            }
+            Console.ReadLine();
+        }
+
+        static void WarningToConsole() {
+            Console.WriteLine("Advarsel");
+        }
+        static void WarningAgain()
+        {
+            Console.WriteLine("Hovsa");
+        }
+    }
+
+    public delegate void Warning();
+
+    public class PowerPlant
+    {
+        Warning w;
+        Random r = new Random();
+
+        public PowerPlant() { }
+
+        public void SetWarning(Warning w)
+        {
+            //this.w = w;
+            this.w += w;
+        }
+
+        public void HeatUp()
+        {
+            if (r.Next(0,100) > 50) {
+                w.Invoke();
+            }
+        }
+    }
+}
---
+++ Opgave2Warning/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Opgave2Warning")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Opgave2Warning")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7ad7a479-2675-4f60-8462-3153dfaaa08a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
