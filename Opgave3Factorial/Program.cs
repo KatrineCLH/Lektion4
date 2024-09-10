5878a58397ba4adee92884bfe59f9393f3628762
katrine <eaa23kch@students.eaaa.dk>
2024-09-10 09:50:44 +0200

    Made a recursive method for calculating Factorial of a number

---
+++ Opgave3Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave3Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+            Console.ReadLine();
+        }
+
+        static int Factorial(int n)
+        {
+            if (n >= 0)
+            {
+                if (n == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return n * Factorial(n - 1);
+                }
+            }
+            else return -1;
+        }
+    }
+}
